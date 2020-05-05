81ecbca266b5d9dc93e23281856f6ada97890125
Wesly <ultimatecracker88@gmail.com>
2020-05-05 18:50:25 -0400

    new file in batch

---
+++ bash_file/myName.bat
@@ -0,0 +1,4 @@
+@echo OFF
+cd mydirectory
+echo hello world > myfile.txt
+echo Hello %1 Wesly %2 Hernandez
---
+++ myName2.bat
@@ -0,0 +1,6 @@
+@echo OFF
+cd mydirectory
+echo hello world > myfile.txt
+echo Hello %1 Wesly %2 Hernandez
+cd .. 
+rmdir /s mydirectory
